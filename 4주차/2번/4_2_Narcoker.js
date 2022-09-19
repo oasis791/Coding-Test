a2a77d8d8311632d862378cbd9cb7098a460feeb
Narcoker <jykim9868@gmail.com>
2022-09-19 17:04:09 +0900

    4주차 2번 문제 리팩토링

--- 4주차/2번/4_2_Narcoker.js
+++ 4주차/2번/4_2_Narcoker.js
@@ -2,7 +2,7 @@
 
 function solution(brown, yellow) {
     let sum = brown + yellow;
-    for (let i = 3; i < sum / 2; i++) {
+    for (let i = 3; i <= Math.sqrt(sum); i++) { // 세로 기준
         if (sum % i === 0) {
             let k = sum / i;
             if ((i - 2) * (k - 2) === yellow) return [k, i];
