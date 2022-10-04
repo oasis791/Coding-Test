0db95707fa92faa360ee526f36b956207e1327be
oasis791 <khwi791@naver.com>
2022-10-04 16:06:08 +0900

    aborting handle

---
+++ 6주차/1번/6_1_oasis791.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            func(n % 3, sb);
+            if (n % 3 == 0) {
+                n = n / 3 - 1;
+            } else {
+                n /= 3;
+            }
+        }
+        return sb.toString();
+    }
+
+    static StringBuilder func(int n, StringBuilder sb) {
+        switch (n) {
+            case 1 -> sb.insert(0, 1);
+            case 2 -> sb.insert(0, 2);
+            case 0 -> sb.insert(0, 4);
+        }
+        return sb;
+    }
+}
