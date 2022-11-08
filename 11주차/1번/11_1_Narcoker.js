dd947db886ba5b9901e3bd967110f0714e7ee3c8
Narcoker <jykim9868@gmail.com>
2022-11-08 21:16:38 +0900

    Solved: 11주차 1번 문제 해결

--- 11주차/1번/11_1_Narcoker.js
+++ 11주차/1번/11_1_Narcoker.js
@@ -1,22 +1,13 @@
-function solution(n, k) {
-    const answer = [];
-    const isVisited = new Array(n).fill(false);
-    function search(isVisited, order) {
-        if (order.length === n) {
-            answer.push(order);
-            return;
+function solution(number) {
+    let answer = 0;
+    function search(sum, index, count) {
+        if (count === 3) {
+            if (!sum) answer++;
+            else return;
         }
-        for (let i = 0; i < n; i++) {
-            if (!isVisited[i]) {
-                let newIsVisited = [...isVisited];
-                let newOrder = [...order];
-                newIsVisited[i] = true;
-                newOrder.push(i + 1);
-                search(newIsVisited, newOrder);
-            }
-        }
+        for (let i = index; i < number.length; i++)
+            search(sum + number[i], i + 1, count + 1);
     }
-
-    search(isVisited, []);
-    return answer[k];
+    search(0, 0, 0);
+    return answer;
 }
