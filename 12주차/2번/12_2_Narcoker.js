ee8ce6c73080a9e3fa5e01d65afddc8f989cba47
Narcoker <jykim9868@gmail.com>
2022-11-11 15:44:03 +0900

    12주차 2번 문제 해결

---
+++ 12주차/2번/12_2_Narcoker.js
@@ -0,0 +1,10 @@
+function solution(n, a, b) {
+    let answer = 0;
+    while (a !== b) {
+        a = Math.floor(a / 2) + a % 2;
+        b = Math.floor(b / 2) + b % 2;
+        answer++;
+    }
+    return answer;
+}
+console.log(solution(8, 4, 7));
