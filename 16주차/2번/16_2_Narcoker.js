ae990f72e49fe84dd853037f19d840329ac3d9f8
Narcoker <jykim9868@gmail.com>
2022-12-13 15:28:18 +0900

    16주차 2번 문제 해결

---
+++ 16주차/2번/16_2_Narcoker.js
@@ -0,0 +1,32 @@
+function solution(n) {
+    function assign(y, x, num, type) {
+        if (type === '0') { // 아래
+            arr[y + 1][x] = num;
+            return [y + 1, x]
+        }
+        if (type === '1') { // 오른쪽
+            arr[y][x + 1] = num;
+            return [y, x + 1];
+        }
+        if (type === '2') { // 위
+            arr[y - 1][x - 1] = num;
+            return [y - 1, x - 1];
+        }
+    }
+    const arr = [];
+    let move = ""
+
+    for (let i = 0; i < n; i++) {
+        arr.push(Array(i + 1).fill(0));
+        move += (i % 3 + "").repeat(n - i);
+    }
+
+    let curY = -1;
+    let curX = 0;
+    let num = 1;
+    for (let i = 0; i < move.length; i++) {
+        [curY, curX] = assign(curY, curX, num, move[i]);
+        num++;
+    }
+    return arr.reduce((res, value) => res.concat(value));
+}
